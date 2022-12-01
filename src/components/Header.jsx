118efc2a0a5006dbf6f928f254a877035c7c4c83
wook@desktop05 <rudollee@gmail.com>
2022-12-01 15:21:14 +0900

    completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className={"row"}>
+            <p className={"page-header"} >Trello Dashboard 👽</p>
+        </div>
+    )
+}
+
+export default Header
